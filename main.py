998d973dc748915a4ac2cf37d74f5c85ec8bf200
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-04 14:56:54 +0600

    sdsdsd

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-data = pd.read_csv("movies_metadata.csv")[0:15000]
-print(data["overview"].head())
+data = pd.read_csv("data.csv")
 
+test_data = data["название"].iloc[22]
 
-tfidf = TfidfVectorizer(stop_words='english')
-data['overview'] = data['overview'].fillna('')
-tfidf_matrix = tfidf.fit_transform(data['overview'])
-print(tfidf_matrix)
-print(tfidf_matrix.shape)
-# print(tfidf.get_feature_names()[5000:5010])
+tfidf = TfidfVectorizer()
+data['Описание'] = data['Описание'].fillna('')
+tfidf_matrix = tfidf.fit_transform(data['название'])
 
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-indices = pd.Series(data.index, index=data['title']).drop_duplicates()
-print(indices.head())
+indices = pd.Series(data.index, index=data['название']).drop_duplicates()
 
 
 def get_recommendations(title, cosine_sim=cosine_sim):
@@ -24,8 +20,12 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    return data['title'].iloc[movie_indices]
+    topMovies = data['название'].iloc[movie_indices]
+    movie_ratings = [i[1] for i in sim_scores]
+    topMovies['Score'] = movie_ratings
+    return topMovies
 
 
-result = get_recommendations("Devil")
+result = get_recommendations(test_data)
+print("результаты")
 print(result)
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-import matplotlib.pyplot as plt;
+import matplotlib.pyplot as plt
 
 plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 from textwrap import wrap
 
-metadata = pd.read_csv('movies_metadata.csv', low_memory=False)[0:15000]
+metadata = pd.read_csv('movies_metadata.csv', low_memory=False)[0:5000]
 
 # Remove all english stop words such as 'the', 'a'
 tfidf = TfidfVectorizer(stop_words='english')
@@ -16,7 +16,7 @@ tfidf = TfidfVectorizer(stop_words='english')
 metadata['overview'] = metadata['overview'].fillna('')
 
 tfidf_matrix = tfidf.fit_transform(metadata['overview'])
-# print(tfidf_matrix.shape)
+print("tfidf_matrix", tfidf_matrix.shape)
 
 # Compute the cosine similarity matrix
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
@@ -24,8 +24,10 @@ cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 # Construct a reverse map of indices and movie titles
 indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
 
+print("indices", indices)
 
-# print(indices)
+print("end indices")
+
 def get_recommendations(title, cosine_sim=cosine_sim):
     # Get the index of the movie that matches the title
     index = indices[title]
@@ -34,6 +36,8 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     sim_scores = list(enumerate(cosine_sim[index]))
 
     # Sort the movies based on the similarity scores
+    print("lambda start")
+
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
     # Get the scores of the 10 most similar movies
@@ -45,7 +49,7 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     topMovies = metadata['title'].iloc[movie_indices]
 
     movie_ratings = [i[1] for i in sim_scores]
-
+    print(topMovies)
     topMovies['Score'] = movie_ratings
 
     # Return the top 10 most similar movies
@@ -55,31 +59,10 @@ def get_recommendations(title, cosine_sim=cosine_sim):
 # topMovies = get_recommendations('Persuasion')
 # topMovies = get_recommendations('The Godfather')
 
-
+print("--------------Top Movies---------------------------")
 topMovies = get_recommendations('Toy Story')
 print(topMovies)
 
 print("--------------Scores---------------------------")
 print(topMovies['Score'])
 
-# Code for bar chart
-
-titles = np.array(topMovies)[0:10]
-score = np.array(topMovies['Score'])
-# print(titles.shape)
-# print(score.shape)
-
-titles = ['\n'.join(wrap(l, 10)) for l in titles]
-
-from pylab import rcParams
-
-rcParams['figure.figsize'] = 40, 20
-
-ind = np.arange(1, len(score) + 1)
-# print(ind)
-plt.title("")
-plt.ylabel("Score")
-plt.xlabel("Movie Title")
-plt.bar(ind, score, tick_label=titles, align='center')
-plt.legend()
-plt.show()
