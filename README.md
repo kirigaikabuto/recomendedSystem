8f4aa5c42a68743a83e4276e7523e23340e284e1
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-04 15:39:18 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # recomendedSystem
+# recommendationApi
---
+++ train_part.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import pickle
+data = pd.read_csv("data.csv")
+
+test_data = data["название"].iloc[22]
+
+tfidf = TfidfVectorizer()
+data['Описание'] = data['Описание'].fillna('')
+tfidf_matrix = tfidf.fit_transform(data['название'])
+pickle.dump(tfidf_matrix, open('tfidf_matrix.pickle', 'wb'))
+
